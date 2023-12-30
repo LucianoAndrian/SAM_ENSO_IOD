c2a951aa179f5abc6bb6f533eacfdbcd9a4f6327
LucianoAndrian <lucianoandrian@hotmail.com>
2023-12-30 20:13:22 -0300

    upd

--- Test_CEN2.py
+++ Test_CEN2.py
@@ -1,39 +1,43 @@
 """
-TEST Algoritmo de descubrimiento causal
-en UN punto de grilla
+Intento simple de usar pcmci en dmi-enso por punto de grilla
 """
 
 ################################################################################
-import warnings
-warnings.filterwarnings( "ignore", module = "matplotlib\..*" )
-import pandas as pd
-pd.options.mode.chained_assignment = None
-import numpy as np
-import xarray as xr
-from ENSO_IOD_Funciones import Nino34CPC, SameDateAs, DMI2
 import os
 os.environ['HDF5_USE_FILE_LOCKING'] = 'FALSE'
+import numpy as np
+import xarray as xr
+import pandas as pd
+pd.options.mode.chained_assignment = None
+
+from ENSO_IOD_Funciones import Nino34CPC, SameDateAs, DMI2
+from PCMCI import PCMCI
+
+import statsmodels.api as sm
+import concurrent.futures
+from datetime import datetime
+import time
+
+import matplotlib.pyplot as plt
+from matplotlib import colors
+import cartopy.feature
+from cartopy.mpl.ticker import LongitudeFormatter, LatitudeFormatter
+import cartopy.crs as ccrs
+
 import warnings
+warnings.filterwarnings( "ignore", module = "matplotlib\..*" )
 from shapely.errors import ShapelyDeprecationWarning
 warnings.filterwarnings("ignore", category=ShapelyDeprecationWarning)
-import statsmodels.api as sm
-from PCMCI import PCMCI
-import concurrent.futures
-import time
-import matplotlib.pyplot as plt
 ################################################################################
+save=False
+use_sam=False
 ################################################################################
-sam_dir = '/pikachu/datos/luciano.andrian/SAM_ENSO_IOD/salidas/'
-out_dir = '/pikachu/datos/luciano.andrian/SAM_ENSO_IOD/salidas/mlr/'
-era5_dir = '/pikachu/datos/luciano.andrian/observado/ncfiles/ERA5/1940_2020/'
-t_pp_dir = '/pikachu/datos/luciano.andrian/observado/ncfiles/data_obs_d_w_c/'
 # ---------------------------------------------------------------------------- #
-path = '/pikachu/datos/luciano.andrian/cases_fields/'
-out_dir = '/pikachu/datos/luciano.andrian/SAM_ENSO_IOD/salidas/eof/'
-################################################################################
-################################################################################
-ruta = '/pikachu/datos/luciano.andrian/observado/ncfiles/ERA5/downloaded/'
-hgt = xr.open_dataset(ruta + 'ERA5_HGT200_40-20.nc')
+sam_dir = '/pikachu/datos/luciano.andrian/SAM_ENSO_IOD/salidas/'
+hgt_dir = '/pikachu/datos/luciano.andrian/observado/ncfiles/ERA5/downloaded/'
+#out_dir = ''
+# ---------------------------------------------------------------------------- #
+hgt = xr.open_dataset(hgt_dir + 'ERA5_HGT200_40-20.nc')
 hgt = hgt.rename({'longitude': 'lon', 'latitude': 'lat', 'z': 'var'})
 hgt = hgt.interp(lon=np.arange(0,360,2), lat=np.arange(-90, 90, 2))
 
@@ -41,14 +45,16 @@ hgt_clim = hgt.sel(time=slice('1979-01-01', '2000-12-01'))
 hgt_anom = hgt.groupby('time.month') - \
            hgt_clim.groupby('time.month').mean('time')
 
-# weights = np.sqrt(np.abs(np.cos(np.radians(hgt_anom.lat))))
-# hgt_anom = hgt_anom * weights
+if use_sam:
+    weights = np.sqrt(np.abs(np.cos(np.radians(hgt_anom.lat))))
+    hgt_anom = hgt_anom * weights
 
 #hgt_anom2 = hgt_anom.sel(lat=slice(-80, 0), lon=slice(60, 70))
 hgt_anom = hgt_anom.rolling(time=3, center=True).mean()
 hgt_anom = hgt_anom.sel(time=slice('1940-02-01', '2020-11-01'))
 hgt_anom = hgt_anom.sel(time=hgt_anom.time.dt.month.isin([8,9,10,11]))
-# ---------------------------------------------------------------------------- #
+################################################################################
+# indices
 # ---------------------------------------------------------------------------- #
 sam = xr.open_dataset(sam_dir + 'sam_700.nc')['mean_estimate']
 sam = sam.rolling(time=3, center=True).mean()
@@ -58,10 +64,7 @@ dmi = DMI2(filter_bwa=False, start_per='1920', end_per='2020',
 
 aux = xr.open_dataset("/pikachu/datos4/Obs/sst/sst.mnmean_2020.nc")
 n34 = Nino34CPC(aux, start=1920, end=2020)[0]
-################################################################################
-#c = hgt_anom2.sel(lon=65, lat=-75)#.sel(time=hgt_anom.time.dt.year.isin(np.arange(1980,2020)))
-#c = c.sel(time=c.time.dt.year.isin([np.arange(2010,2018)]))
-#c = xr.open_dataset(sam_dir + 'sam_700.nc')['mean_estimate']
+# ---------------------------------------------------------------------------- #
 dmi2 = SameDateAs(dmi, hgt_anom)
 n342 = SameDateAs(n34, hgt_anom)
 sam2 = SameDateAs(sam, hgt_anom)
@@ -89,6 +92,7 @@ def recursive_cut(s1, s2):
 
     return s1, s2
 
+
 def min_len(st):
     min_len = float('inf')
     min_st = None
@@ -100,6 +104,7 @@ def min_len(st):
             min_st = s
 
     return min_st
+
 
 def SetLags(x, y, ty, series, parents):
 
@@ -141,13 +146,14 @@ def SetLags(x, y, ty, series, parents):
 
     return pd.DataFrame({'x':x, 'y':y, **z_data})
 
-# def regre_res(df):
-#     import statsmodels.api as sm
+
+# def regre(df):
 #
 #     x_model = sm.OLS(df['y'], sm.add_constant(df[df.columns[2:]])).fit()
 #     x_res = x_model.resid
 #
-#     return x_res
+#     return x_model.predict()
+
 
 def regre(df):
     X = np.column_stack((np.ones_like(df[df.columns[2:]]), df[df.columns[2:]]))
@@ -162,6 +168,7 @@ def regre(df):
     #return x_res
     return  np.dot(X, beta)
 
+
 def regre_res(df):
     X = np.column_stack((np.ones_like(df[df.columns[2:]]), df[df.columns[2:]]))
     y = df['x']
@@ -171,8 +178,8 @@ def regre_res(df):
 
     # Calcular los residuos
     x_res = y - np.dot(X, beta)
-
     return x_res
+
 
 def resize_serie(s1, len_min):
     i=1
@@ -182,15 +189,18 @@ def resize_serie(s1, len_min):
         if i > 100:
             print('Error recursive_cut +100 iter.')
             return
-
     return s1
 
-################################################################################
-def aux_func(x):
-    target='n34'
+
+def Compute_PCMCI_MLR(x):
+    target='dmi'
+    dc2020=True
     lag=0
+    # PENDIENTE
+    # SETEAR LAAAG!
     x_values = x
     series = {'c': x_values, 'dmi': dmi3.values, 'n34': n343.values}
+
     result_df = PCMCI(series=series, tau_max=2, pc_alpha=0.2, mci_alpha=0.05)
 
     actors_parents=[]
@@ -198,7 +208,7 @@ def aux_func(x):
         aux_df = result_df.loc[result_df['Target'] == actor]
         links = list(aux_df['Actor'])
 
-
+        # Solo autocorrelacion
         own_links = []
         for element in links:
             if actor in element:
@@ -209,47 +219,57 @@ def aux_func(x):
                      series=series, parents=own_links)
 
         if actor == 'c':
-            actors_parents.append(regre(df))
+            actors_parents.append(regre(df)) #0
         elif actor == target:
-            actors_parents.append(regre(df))
-            actors_parents.append(regre_res(df))
+            actors_parents.append(regre(df)) #1
+            actors_parents.append(regre_res(df)) #2
         else:
-            actors_parents.append(df['x'].values)
-            actors_parents.append(regre(df))
+            actors_parents.append(df['x'].values) #3
+            actors_parents.append(regre(df)) # 4
+            actors_parents.append(regre_res(df)) #5
 
-        # actors_parents.append(regre_res(df))
 
+    # longitud de las series =
     len_min = min(len(serie) for serie in actors_parents)
-
     for i, a_res in enumerate(actors_parents):
         actors_parents[i] = resize_serie(a_res, len_min)
 
-    # crear dos formas,
-    # forma 2
-    aux_df = pd.DataFrame({'c_or': resize_serie(series['c'], len_min),
-                           target: resize_serie(series[target], len_min),
-                           'c_t': actors_parents[0],
-                           target+'_t': actors_parents[1],
-                           '2actor': actors_parents[3],
-                           '2actor_t':actors_parents[4]})
-    #,
-                           #'c_l': actors_parents[0].values,
-                           #target +'_l': actors_parents[1],
-                           #'actor_WO': actors_parents[3]
-                           #'n34_res':actors_parents[2].values})
+    # CEN
+    if dc2020:
+        aux_df = pd.DataFrame({'c_or': resize_serie(series['c'], len_min),
+                               target: resize_serie(series[target], len_min),
+                               target + '_t': actors_parents[1],
+                               'c_t': actors_parents[0],
+                               '2actor': actors_parents[3]
+                               })
+    else:
+        # set
+        aux_df = pd.DataFrame({'c_or': resize_serie(series['c'], len_min),
+                               # target:actors_parents[2],
+                               target: resize_serie(series[target], len_min),
+                               'n34': resize_serie(series['n34'], len_min),
+                               'c_t': actors_parents[0],
+                               target + '_t': actors_parents[1],
+                               # '2actor': actors_parents[3],
+                               '2actor_t': actors_parents[4],
+                               })
+        # 'c_l': actors_parents[0].values,
+        # target +'_l': actors_parents[1],
+        # 'actor_WO': actors_parents[3]
+        # 'n34_res':actors_parents[2].values})
 
-    model = sm.OLS(aux_df['c_or'],
-                   sm.add_constant(aux_df[aux_df.columns[1:]])).fit()
+    model = sm.OLS(aux_df['c_or'], aux_df[aux_df.columns[1:]]).fit()
 
-    if model.pvalues[1]<0.5:
-        return model.params[1]
+    if model.pvalues[0]<0.05:
+        return model.params[0]
     else:
         return np.nan
 
-def fakedaks(c):
+
+def ComputeByXrUf(c):
     hgt_anom2 = hgt_anom.sel(lat=slice(c[0], c[1]), lon=slice(c[2], c[3]))
     reg_array = xr.apply_ufunc(
-        aux_func,
+        Compute_PCMCI_MLR,
         hgt_anom2['var'],
         input_core_dims=[['time']],
         # dask='parallelized',
@@ -257,10 +277,8 @@ def fakedaks(c):
         output_dtypes=[float])
     return reg_array
 
-
-
-
-# hacer funcion para esto
+################################################################################
+# Subregiones a computar en paralelo
 lonlat = [[-80, -60, 30, 60], [-80, -60, 61, 90], [-80, -60, 91, 120],
           [-80, -60, 121, 150], [-80, -60, 151, 180], [-80, -60, 181, 210],
           [-80, -60, 211, 240], [-80, -60, 241, 270], [-80, -60, 271, 300],
@@ -274,22 +292,23 @@ lonlat = [[-80, -60, 30, 60], [-80, -60, 61, 90], [-80, -60, 91, 120],
           [-31, 0, 211, 240], [-31, 0, 241, 270], [-31, 0, 271, 300],
           [-31, 0, 301, 330]]
 
-
-from datetime import datetime
-print(datetime.now())
+# Fake Dask ------------------------------------------------------------------ #
+print(f'Hora de inicio: {datetime.now()}')
 time0 = time.time()
 
-with concurrent.futures.ProcessPoolExecutor(max_workers=10) as executor:
-    aux_result = list(executor.map(fakedaks, lonlat))
+with concurrent.futures.ProcessPoolExecutor(max_workers=20) as executor:
+    aux_result = list(executor.map(ComputeByXrUf, lonlat))
 
 delta_t = time.time() - time0
 print(f"Tiempo: {delta_t/60} minutos")
 
+# uniendo las regiones calculadas en paralelo
 aux_xr = xr.merge(aux_result)
 
-from matplotlib import colors
-import numpy as np
-################################################################################
+# ---------------------------------------------------------------------------- #
+# ---------------------------------------------------------------------------- #
+# plot
+# ---------------------------------------------------------------------------- #
 cbar_hgt = colors.ListedColormap(['#9B1C00', '#B9391B', '#CD4838',
                                       '#E25E55', '#F28C89', '#FFCECC',
                                       'white',
@@ -299,27 +318,21 @@ cbar_hgt.set_over('#641B00')
 cbar_hgt.set_under('#012A52')
 cbar_hgt.set_bad(color='white')
 
-
-import cartopy.feature
-from cartopy.mpl.ticker import LongitudeFormatter, LatitudeFormatter
-import cartopy.crs as ccrs
-
 fig = plt.figure(figsize=(7, 3), dpi=100)
 ax = plt.axes(projection=ccrs.PlateCarree(central_longitude=180))
 crs_latlon = ccrs.PlateCarree()
 
-ax.set_extent([0, 359, -90, 10], crs=crs_latlon)
+ax.set_extent([30, 330, -80, 0], crs=crs_latlon)
 
 im = ax.contourf(aux_xr.lon, aux_xr.lat, aux_xr['var'],
-                 # levels=[-0.2, -0.1, -0.05, 0, 0.05, 0.1, 0.2],
                  levels=[-150, -100, -75, -50, -25, -15,
                          0, 15, 25, 50, 75, 100, 150],
                  transform=crs_latlon, cmap=cbar_hgt, extend='both')
 
 values = ax.contour(aux_xr.lon, aux_xr.lat, aux_xr['var'],
-                    levels=np.arange(-350, 350, 50),
+                    levels=[-150, -100, -75, -50, -25, -15,
+                         0, 15, 25, 50, 75, 100, 150],
                     transform=crs_latlon, colors='k', linewidths=1)
-
 cb = plt.colorbar(im, fraction=0.042, pad=0.035, shrink=0.8)
 cb.ax.tick_params(labelsize=8)
 ax.add_feature(cartopy.feature.LAND, facecolor='#d9d9d9')
@@ -331,18 +344,12 @@ ax.xaxis.set_major_formatter(lon_formatter)
 ax.yaxis.set_major_formatter(lat_formatter)
 ax.tick_params(labelsize=7)
 plt.title('test', fontsize=10)
-# if text:
-#     plt.figtext(0.5, 0.01, number_events, ha="center", fontsize=10,
-#                 bbox={'facecolor': 'w', 'alpha': 0.5, 'pad': 5})
 plt.tight_layout()
-
-# save=False
-# if save:
-#     plt.savefig(
-#         name_fig + str(season) + '_' + str(fase.split(' ', 1)[1]) + '.jpg')
-#     plt.close()
-# else:
-#     plt.show()
+if save:
+    plt.savefig('namefig.jpg')
+    plt.close()
+else:
+    plt.show()
 plt.show()
 
 
