5b752fe2114b456ae22b05c1c3958d01af4fc2bb
LucianoAndrian <lucianoandrian@hotmail.com>
2024-02-14 18:54:47 -0300

    done select fields

--- CFSv2_SelectVariables.py
+++ CFSv2_SelectVariables.py
@@ -2,103 +2,190 @@
 Similar al SelectEvents de ENSO_IOD
 """
 # ---------------------------------------------------------------------------- #
-save = False
-# ---------------------------------------------------------------------------- #
-path = '/pikachu/datos/luciano.andrian/SAM_ENSO_IOD/salidas/eof/'
-path_aux = '/pikachu/datos/luciano.andrian/cases_fields/'
-out_dir = '/pikachu/datos/luciano.andrian/SAM_ENSO_IOD/salidas/events_dates/'
+data_dir = '/pikachu/datos/luciano.andrian/cases_fields/'
+cases_date_dir = '/pikachu/datos/luciano.andrian/SAM_ENSO_IOD/salidas/' \
+                 'events_dates/'
+out_dir = '/pikachu/datos/luciano.andrian/SAM_ENSO_IOD/salidas/cases_fields/'
 # ---------------------------------------------------------------------------- #
 import xarray as xr
-import numpy as np
-import asyncio # **
-# **
-# La selección de los campos en hgt tarda mucho debido a la cantidad >10mil
-# en general para cada categoria.
-# Al hacerlo de manera asincronica en paralelo es 7 veces mas rapido.
-# No se con cuantos hilos opera, en htop no se ve. Ni como configurarlos7
-# consumo de RAM, bajo
+from multiprocessing.pool import ThreadPool
+from multiprocessing import Process
+from ENSO_IOD_Funciones import SelectVariables
+
+cases = ['sim_pos', 'sim_neg', #triple sim
+         'dmi_puros_pos', 'dmi_puros_neg', # puros, sin los otros dos
+         'n34_puros_pos', 'n34_puros_neg', # puros, sin los otros dos
+         'sam_puros_pos', 'sam_puros_neg', # puros, sin los otros dos
+         'dmi_pos_n34_pos_sam_neg', # simultaneo signos opuestos
+         'dmi_pos_n34_neg_sam_pos', # simultaneo signos opuestos
+         'dmi_pos_n34_neg_sam_neg', # simultaneo signos opuestos
+         'dmi_neg_n34_pos_sam_neg', # simultaneo signos opuestos
+         'dmi_neg_n34_pos_sam_pos', # simultaneo signos opuestos
+         'dmi_neg_n34_neg_sam_pos', # simultaneo signos opuestos
+         'dmi_pos', 'dmi_neg', # todos los de uno, sin importar el resto
+         'n34_pos', 'n34_neg', # todos los de uno, sin importar el resto
+         'sam_pos', 'sam_neg', # todos los de uno, sin importar el resto
+         'neutros'] # todos neutros
+
+seasons = ['SON']
 # ---------------------------------------------------------------------------- #
-sam_index = xr.open_dataset(path + 'sam_rmon_r_z200.nc').\
-    rename({'time2':'time'})
-hgt = xr.open_dataset(path_aux + 'hgt_mon_anom_d.nc')
-# ---------------------------------------------------------------------------- #
-def SelectEvents(indice, operador, umbral='0', abs_val=False):
-    runs = np.arange(1, 25)
-    dates = indice.time.values
-    leads = [0, 1, 2, 3]
+# SST ##########################################################################
+if len(seasons)>1:
+    def SelectEvents(c):
+        for s in seasons:
+            aux_cases = xr.open_dataset(
+                cases_date_dir + c + '_f_' + s + '.nc')\
+                .rename({'__xarray_dataarray_variable__': 'index'})
+            data_sst_s = xr.open_dataset(data_dir + 'sst_' + s.lower() + '.nc')
+            case_events = SelectVariables(aux_cases, data_sst_s)
+            case_events.to_netcdf(out_dir + c + '_' + s + '.nc')
 
-    first = True
-    for l in leads:
-        for r in runs:
-            for d in dates:
-                aux = sam_index.sel(L=l, r=r, time=d)
-                expresion = f"aux.pcs.values {operador} {umbral}"
-                if abs_val:
-                    expresion = f"np.abs(aux.pcs.values) {operador} {umbral}"
-                if eval(expresion):
-                    aux = aux.assign_coords(L=l)
-                    if first:
-                        first = False
-                        sam_selected = aux
-                    else:
-                        sam_selected = xr.concat([sam_selected, aux],
-                                                 dim='time')
 
-    sam_selected = sam_selected.rename({'pcs':'sam'})
-    sam_selected = sam_selected.drop('mode')
+    pool = ThreadPool(4)  # uno por season
+    pool.map_async(SelectEvents, [c for c in cases])
+else:
+    print('one season')
+    def SelectEvents(c):
+        s=seasons[0]
+        aux_cases = xr.open_dataset(cases_date_dir + c + '_f_' + s + '.nc') \
+            .rename({'__xarray_dataarray_variable__': 'index'})
+        data_sst_s = xr.open_dataset(data_dir + 'sst_' + s.lower() + '.nc')
+        case_events = SelectVariables(aux_cases, data_sst_s)
+        case_events.to_netcdf(out_dir + c + '_' + s + '.nc')
 
-    return sam_selected
 
-async def SelectFields(event_time):
-    data_field = hgt
+    processes = [Process(target=SelectEvents, args=(c,)) for c in cases]
+    for process in processes:
+        process.start()
 
-    d = event_time[0]
-    l = event_time[1]
-    r = event_time[2]
+# HGT ##########################################################################
+if len(seasons)>1:
+    def SelectEventsHGT(c):
+        for s in seasons:
+            try:
+                aux_cases = xr.open_dataset(
+                    cases_date_dir + c + '_f_' + s + '.nc').\
+                    rename({'__xarray_dataarray_variable__': 'index'})
+            except:
+                aux_cases = xr.open_dataset(
+                    cases_date_dir + c + '_f_' + s + '.nc')\
+                    .rename({'sst': 'index'})
 
-    aux = data_field.sel(time=data_field['L']==l)
-    aux = aux.sel(time=d, r=r)
+            data_hgt_s = xr.open_dataset(data_dir + 'hgt_' + s.lower() + '.nc')
+            case_events = SelectVariables(aux_cases, data_hgt_s)
 
-    return aux
+            case_events.to_netcdf(out_dir + 'hgt_' + c + '_' + s + '.nc')
 
-def SetOrder(dataset):
-    t = dataset['time'].values
-    L = dataset['L'].values
-    r = dataset['r'].values
+    pool = ThreadPool(4)  # uno por season
+    pool.map_async(SelectEventsHGT, [c for c in cases])
 
-    values = [[t[i], L[i], r[i]] for i in range(len(t))]
+else:
+    print('one season')
+    def SelectEventsHGT(c):
+        s = seasons[0]
+        try:
+            aux_cases = xr.open_dataset(
+                cases_date_dir + c + '_f_' + s + '.nc')\
+                .rename({'__xarray_dataarray_variable__': 'index'})
+        except:
+            aux_cases = xr.open_dataset(
+                cases_date_dir + c + '_f_' + s + '.nc')\
+                .rename({'sst': 'index'})
 
-    return values
+        data_hgt_s = xr.open_dataset(data_dir + 'hgt_' + s.lower() + '.nc')
 
-async def main(index_selected):
-    datos = SetOrder(index_selected)
-    tareas = [SelectFields(event_time) for event_time in datos]
+        case_events = SelectVariables(aux_cases, data_hgt_s)
+        case_events.to_netcdf(out_dir + 'hgt_' + c + '_' + s + '.nc')
 
-    resultados = await asyncio.gather(*tareas)
 
-    return resultados
-# ---------------------------------------------------------------------------- #
-# Determinar categorias <, >, "neutro" y umbrales.
-sam_neutro = SelectEvents(sam_index, '<', '0.5', True)
-resultados = asyncio.run(main(sam_neutro))
-aux_xr = xr.concat(resultados, dim='time')
-if save:
-    aux_xr.to_netcdf(out_dir + 'sam_neutro_hgt200.nc')
-    sam_neutro.to_netcdf(out_dir + 'sam_neutro.nc')
-
-sam_pos = SelectEvents(sam_index, '>', '0.5')
-resultados = asyncio.run(main(sam_pos))
-aux_xr = xr.concat(resultados, dim='time')
-if save:
-    aux_xr.to_netcdf(out_dir + 'sam_pos_hgt200.nc')
-    sam_pos.to_netcdf(out_dir + 'sam_pos.nc')
-
-sam_neg = SelectEvents(sam_index, '<', '-0.5')
-resultados = asyncio.run(main(sam_neg))
-aux_xr = xr.concat(resultados, dim='time')
-if save:
-    aux_xr.to_netcdf(out_dir + 'sam_neg_hgt200.nc')
-    sam_neg.to_netcdf(out_dir + 'sam_neg.nc')
+    processes = [Process(target=SelectEventsHGT, args=(c,)) for c in cases]
+    for process in processes:
+        process.start()
 
 # ---------------------------------------------------------------------------- #
 # ---------------------------------------------------------------------------- #
+
+# sam_index = xr.open_dataset(path + 'sam_rmon_r_z200.nc').\
+#     rename({'time2':'time'})
+# hgt = xr.open_dataset(path_aux + 'hgt_mon_anom_d.nc')
+#
+# def SelectEvents(indice, operador, umbral='0', abs_val=False):
+#     runs = np.arange(1, 25)
+#     dates = indice.time.values
+#     leads = [0, 1, 2, 3]
+#
+#     first = True
+#     for l in leads:
+#         for r in runs:
+#             for d in dates:
+#                 aux = sam_index.sel(L=l, r=r, time=d)
+#                 expresion = f"aux.pcs.values {operador} {umbral}"
+#                 if abs_val:
+#                     expresion = f"np.abs(aux.pcs.values) {operador} {umbral}"
+#                 if eval(expresion):
+#                     aux = aux.assign_coords(L=l)
+#                     if first:
+#                         first = False
+#                         sam_selected = aux
+#                     else:
+#                         sam_selected = xr.concat([sam_selected, aux],
+#                                                  dim='time')
+#
+#     sam_selected = sam_selected.rename({'pcs':'sam'})
+#     sam_selected = sam_selected.drop('mode')
+#
+#     return sam_selected
+#
+# async def SelectFields(event_time):
+#     data_field = hgt
+#
+#     d = event_time[0]
+#     l = event_time[1]
+#     r = event_time[2]
+#
+#     aux = data_field.sel(time=data_field['L']==l)
+#     aux = aux.sel(time=d, r=r)
+#
+#     return aux
+#
+# def SetOrder(dataset):
+#     t = dataset['time'].values
+#     L = dataset['L'].values
+#     r = dataset['r'].values
+#
+#     values = [[t[i], L[i], r[i]] for i in range(len(t))]
+#
+#     return values
+#
+# async def main(index_selected):
+#     datos = SetOrder(index_selected)
+#     tareas = [SelectFields(event_time) for event_time in datos]
+#
+#     resultados = await asyncio.gather(*tareas)
+#
+#     return resultados
+# # ---------------------------------------------------------------------------- #
+# # Determinar categorias <, >, "neutro" y umbrales.
+# sam_neutro = SelectEvents(sam_index, '<', '0.5', True)
+# resultados = asyncio.run(main(sam_neutro))
+# aux_xr = xr.concat(resultados, dim='time')
+# if save:
+#     aux_xr.to_netcdf(out_dir + 'sam_neutro_hgt200.nc')
+#     sam_neutro.to_netcdf(out_dir + 'sam_neutro.nc')
+#
+# sam_pos = SelectEvents(sam_index, '>', '0.5')
+# resultados = asyncio.run(main(sam_pos))
+# aux_xr = xr.concat(resultados, dim='time')
+# if save:
+#     aux_xr.to_netcdf(out_dir + 'sam_pos_hgt200.nc')
+#     sam_pos.to_netcdf(out_dir + 'sam_pos.nc')
+#
+# sam_neg = SelectEvents(sam_index, '<', '-0.5')
+# resultados = asyncio.run(main(sam_neg))
+# aux_xr = xr.concat(resultados, dim='time')
+# if save:
+#     aux_xr.to_netcdf(out_dir + 'sam_neg_hgt200.nc')
+#     sam_neg.to_netcdf(out_dir + 'sam_neg.nc')
+#
+# # ---------------------------------------------------------------------------- #
+# # ---------------------------------------------------------------------------- #
