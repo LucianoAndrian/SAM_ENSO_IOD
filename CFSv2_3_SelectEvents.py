e6dad0643a45b61c02880a509df11e48d9f22dd9
Luciano Andrian <lucianoandrian@hotmail.com>
2025-10-08 16:48:27 -0300

    update CFSv2_3

--- CFSv2_3_SelectEvents.py
+++ CFSv2_3_SelectEvents.py
@@ -1,14 +1,17 @@
 """
 Select Events DMI, N34 y SAM
 """
+# ---------------------------------------------------------------------------- #
 out_dir = '/pikachu/datos/luciano.andrian/SAM_ENSO_IOD/events/'
 save = True
+
 # ---------------------------------------------------------------------------- #
 import xarray as xr
 from funciones.SelectEvents_utils import main_SelectEvents
 
 # ---------------------------------------------------------------------------- #
 data_dir = '/pikachu/datos/luciano.andrian/DMI_N34_Leads_r/'
+
 # Funciones ------------------------------------------------------------------ #
 def OpenAndSetIndice(dates_dir, name_file):
     indice = xr.open_dataset(dates_dir + name_file)
@@ -26,4 +29,5 @@ variables = ['dmi', 'n34', 'sam']
 main_SelectEvents(variables, ds1=dmi, ds2=n34, ds3=sam, thr=0.5,
                   save=save, out_dir=out_dir,
                   season_name='SON',  prefix_file='CFSv2')
+
 # ---------------------------------------------------------------------------- #
